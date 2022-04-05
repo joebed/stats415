4e2df8f16c70d832082397cd2195d860cc6d9cd5
joey <bednarjoey@gmail.com>
2022-04-05 12:23:36 -0400

    added data to csv files, predictors now scaled

--- set_up_data.R
+++ set_up_data.R
@@ -1,10 +1,15 @@
 # In order to use this, use setwd({final_project_directory})
 # getwd() gets your current working directory if you're unsure where you are at
 
+# Clean workspace beforehand
+rm(list=ls())
+
 # Data of all years has these names
 files = c("/TCHOL.XPT", "/SMQ.XPT", "/DR1TOT.XPT", "/DEMO.XPT", "/BPX.XPT", "/BMX.XPT")
 
 library(haven)
+library(dplyr)
+
 
 years_data = list()
 
@@ -41,17 +46,26 @@ for(i in seq(1, 5)){
   
 }
 
-# Remove any column where there is an NA
-years_data = years_data[, colSums(is.na(years_data)) == 0]
-
 # Set train and test data
 train_data = read.csv("data/train.csv")
 test_data = read.csv("data/test.csv")
 train_data = merge(train_data, years_data, all.x = TRUE, by="SEQN")
 test_data = merge(test_data, years_data, all.x = TRUE, by="SEQN")
 
+# Remove any column where there is an NA
+train_data = train_data[, colSums(is.na(train_data)) == 0]
+test_data = test_data[, colSums(is.na(test_data)) == 0]
 
-# Clean workspace
-rm(c("year_data", "years_data", "val", "d", "dir", "i", "j", "year"))
+# Remove columns as outlined in group guidelines
+drops = c("SMDUPCA", "SMD100BR", "DR1DRSTZ", "DRABF", "RIDSTATR")
+train_data = train_data[, !(names(train_data) %in% drops)]
+test_data = test_data[, !(names(test_data) %in% drops)]
+
+# Standardize data
+train_data[seq(3, 144)] = scale(train_data[seq(3, 144)])
+test_data[seq(2, 144)] = scale(test_data[seq(2, 144)])
+
+# Clean data
+rm(list = c("d", "dir", "files", "i", "j", "year", "year_data", "years_data", "drops", "val"))
 
 
