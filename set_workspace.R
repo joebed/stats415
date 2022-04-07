8c1a6ebac2c15aab3194bd42dff31048622c5823
joey <bednarjoey@gmail.com>
2022-04-07 09:48:52 -0400

    added script to load test and train data into workspace

---
+++ set_workspace.R
@@ -0,0 +1,13 @@
+# Set up workspace for the final project
+
+rm(list=ls())
+
+# Set working directory. Just hit enter if you're already on it
+getwd()
+wd = readline(prompt="What is your final project directory?")
+setwd(wd)
+
+train_data = read.csv("train_data.csv")
+test_data = read.csv("test_data.csv")
+
+rm(wd)
