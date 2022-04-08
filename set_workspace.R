7fa7bdf9c5f5b476c1ec808d8d889bdf8e62bbd3
luciaq <luciaq@umich.edu>
2022-04-08 13:39:01 -0400

    some eda added

--- set_workspace.R
+++ set_workspace.R
@@ -4,10 +4,13 @@ rm(list=ls())
 
 # Set working directory. Just hit enter if you're already on it
 getwd()
+
 wd = readline(prompt="What is your final project directory?")
 setwd(wd)
+
 
 train_data = read.csv("train_data.csv")
 test_data = read.csv("test_data.csv")
 
 rm(wd)
+
